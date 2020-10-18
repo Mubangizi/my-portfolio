e1a69ec090a55b9f018f6c5ea0b792a1483269af
mubangizi <mubangizia22@gmail.com>
2020-10-18 16:46:46 +0300

    Add homepage info

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import "./Home.css";
 import SideBar from "../SideBar";
+import Layout from "../Layout";
 
 function Home() {
   return (
-    <div className="Home">
-      <SideBar />
+    <Layout>
       <div className="HomeMainContent">
-        <h1>Welcome To My Page</h1>
+        <h1>Hi, I am Mubangizi Allan</h1>
+        <p>
+          I am a fullstack web developer. I can provide clean code and pixel
+          perfect design. I also make website more & more interactive with web
+          animations.
+        </p>
+        <div className="MySocial"></div>
       </div>
-    </div>
+    </Layout>
   );
 }
 
---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,14 @@
+import React, { Children } from "react";
+import SideBar from "../SideBar";
+import "./Layout.css";
+
+const Layout = ({ children }) => {
+  return (
+    <div className="Layout">
+      <SideBar />
+      <main>{children}</main>
+    </div>
+  );
+};
+
+export default Layout;
