fd818fe2502bc099008525553f6d66e52d82eb45
mubangizi <mubangizia22@gmail.com>
2020-10-24 20:47:52 +0300

    Add Skills component to resume

--- src/assets/data.js
+++ src/assets/data.js
@@ -31,3 +31,30 @@ export const learning = [
       " Lorem ipsum dolor sit amet consectetur, adipisicing elit.Quas, magni mollitia, aspernatur consequatur accusamusvero eum facere exercitationem velit suscipit ipsamplaceat libero.",
   },
 ];
+
+export const skills = [
+  {
+    name: "HTML5",
+    percentage: 95,
+  },
+  {
+    name: "CSS3",
+    percentage: 90,
+  },
+  {
+    name: "Javascript",
+    percentage: 70,
+  },
+  {
+    name: "Python",
+    percentage: 95,
+  },
+  {
+    name: "Reactjs",
+    percentage: 80,
+  },
+  {
+    name: "Nextjs",
+    percentage: 70,
+  },
+];
--- src/components/Resume/index.jsx
+++ src/components/Resume/index.jsx
@@ -9,63 +9,83 @@ import TimelineContent from "@material-ui/lab/TimelineContent";
 import TimelineDot from "@material-ui/lab/TimelineDot";
 import WorkOutlineIcon from "@material-ui/icons/WorkOutline";
 import AssignmentOutlinedIcon from "@material-ui/icons/AssignmentOutlined";
-import { experiences, learning } from "../../assets/data";
+import { experiences, learning, skills } from "../../assets/data";
+import LinearProgress from "@material-ui/core/LinearProgress";
 const Resume = () => {
   return (
     <Layout>
       <div className="Resume">
-        <h1 className="SectionHeader resume">Resume</h1>
-        <div className="ResumePath">
-          <h2 className="ResumeTitle">
-            <WorkOutlineIcon />
-            Working Exerience
-          </h2>
-          <Timeline>
-            {experiences.map((experience) => (
-              <TimelineItem>
-                <TimelineSeparator>
-                  <TimelineDot />
-                  <TimelineConnector />
-                </TimelineSeparator>
-                <TimelineContent>
-                  <div className="TimePath">
-                    <div className="PathDateTime">{experience.time}</div>
-                    <div className="OtherContent">
-                      <h2 className="PathHeader">{experience.title}</h2>
-                      <h4 className="PathCompany">{experience.company}</h4>
-                      <p className="PathInfo">{experience.resposibility}</p>
-                    </div>
-                  </div>
-                </TimelineContent>
-              </TimelineItem>
+        <div className="SkillsInfo">
+          <h1 className="SectionHeader skills">My Skills</h1>
+          <div className="SkillsList">
+            {skills.map((skill) => (
+              <div className="SkillItem">
+                <h3>{skill.name}</h3>
+                <div className="SkillsBar">
+                  <p className="SkillPercentage">{`${skill.percentage} %`}</p>
+                  <LinearProgress
+                    variant="determinate"
+                    value={skill.percentage}
+                  />
+                </div>
+              </div>
             ))}
-          </Timeline>
+          </div>
         </div>
-        <div className="ResumePath">
-          <h2 className="ResumeTitle">
-            <AssignmentOutlinedIcon />
-            Educational Qualifications
-          </h2>
-          <Timeline>
-            {learning.map((step) => (
-              <TimelineItem>
-                <TimelineSeparator>
-                  <TimelineDot />
-                  <TimelineConnector />
-                </TimelineSeparator>
-                <TimelineContent>
-                  <div className="TimePath">
-                    <div className="PathDateTime">{step.time}</div>
-                    <div className="OtherContent">
-                      <h2 className="PathHeader">{step.title}</h2>
-                      <h4 className="PathCompany">{step.company}</h4>
-                      <p className="PathInfo">{step.resposibility}</p>
+        <div className="ResumeInfo">
+          <h1 className="SectionHeader resume">Resume</h1>
+          <div className="ResumePath">
+            <div className="ResumeTitle">
+              <WorkOutlineIcon />
+              <h2>Working Exerience</h2>
+            </div>
+            <Timeline>
+              {experiences.map((experience) => (
+                <TimelineItem>
+                  <TimelineSeparator>
+                    <TimelineDot />
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent>
+                    <div className="TimePath">
+                      <div className="PathDateTime">{experience.time}</div>
+                      <div className="OtherContent">
+                        <h2 className="PathHeader">{experience.title}</h2>
+                        <h4 className="PathCompany">{experience.company}</h4>
+                        <p className="PathInfo">{experience.resposibility}</p>
+                      </div>
                     </div>
-                  </div>
-                </TimelineContent>
-              </TimelineItem>
-            ))}
-          </Timeline>
+                  </TimelineContent>
+                </TimelineItem>
+              ))}
+            </Timeline>
+          </div>
+          <div className="ResumePath">
+            <div className="ResumeTitle">
+              <AssignmentOutlinedIcon />
+              <h2>Educational Qualifications</h2>
+            </div>
+            <Timeline>
+              {learning.map((step) => (
+                <TimelineItem>
+                  <TimelineSeparator>
+                    <TimelineDot />
+                    <TimelineConnector />
+                  </TimelineSeparator>
+                  <TimelineContent>
+                    <div className="TimePath">
+                      <div className="PathDateTime">{step.time}</div>
+                      <div className="OtherContent">
+                        <h2 className="PathHeader">{step.title}</h2>
+                        <h4 className="PathCompany">{step.company}</h4>
+                        <p className="PathInfo">{step.resposibility}</p>
+                      </div>
+                    </div>
+                  </TimelineContent>
+                </TimelineItem>
+              ))}
+            </Timeline>
+          </div>
         </div>
       </div>
     </Layout>
