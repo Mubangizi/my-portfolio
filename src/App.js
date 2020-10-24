3a842bb4c4c380f6c9ec8c743c3c2b240674a57c
mubangizi <mubangizia22@gmail.com>
2020-10-24 22:45:57 +0300

    Add contact Info

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import AboutPage from "./components/AboutPage";
 import HomePage from "./components/HomePage";
 import Portfolio from "./components/Portfolio";
 import Resume from "./components/Resume";
+import Contact from "./components/Contact";
 const App = () => {
   return (
     <BrowserRouter>
@@ -12,6 +13,7 @@ const App = () => {
         <Route exact path="/about" component={AboutPage} />
         <Route exact path="/portfolio" component={Portfolio} />
         <Route exact path="/resume" component={Resume} />
+        <Route exact path="/contact" component={Contact} />
       </Switch>
     </BrowserRouter>
   );
---
+++ src/components/Contact/index.jsx
@@ -0,0 +1,82 @@
+import { Button, TextField } from "@material-ui/core";
+import React from "react";
+import Layout from "../Layout";
+import CallOutlinedIcon from "@material-ui/icons/CallOutlined";
+import "./Contact.css";
+import EmailOutlinedIcon from "@material-ui/icons/EmailOutlined";
+import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
+const Contact = () => {
+  return (
+    <Layout>
+      <div className="Contact">
+        <h1 className="SectionHeader contact">Contact Me</h1>
+        <div className="ContactArea">
+          <div className="SendMailArea">
+            <h2>Get In Touch</h2>
+            <form className="ContactForm">
+              <TextField
+                id="outlined-error"
+                label="Enter Your Name"
+                defaultValue=""
+                variant="outlined"
+              />
+              <TextField
+                id="outlined-error"
+                label="Enter Your Email"
+                defaultValue=""
+                variant="outlined"
+              />
+              <TextField
+                id="outlined-error"
+                label="Enter Your Subject"
+                defaultValue=""
+                variant="outlined"
+              />
+              <TextField
+                className="MessageField"
+                id="outlined-error"
+                label="Enter Your Message"
+                multiline
+                rows={10}
+                defaultValue=""
+                variant="outlined"
+              />
+              <Button>SEND MAIL</Button>
+            </form>
+          </div>
+          <div className="OtherContactInfo">
+            <div className="ContactCard">
+              <span className="ContactCardIcon">
+                <CallOutlinedIcon />
+              </span>
+              <div className="ContactCardInfo">
+                <h3>Phone</h3>
+                <p>+256705754073</p>
+              </div>
+            </div>
+            <div className="ContactCard">
+              <span className="ContactCardIcon">
+                <EmailOutlinedIcon />
+              </span>
+              <div className="ContactCardInfo">
+                <h3>Email</h3>
+                <p>mubangizia22@gmail.com</p>
+              </div>
+            </div>
+            <div className="ContactCard">
+              <span className="ContactCardIcon">
+                <LocationOnOutlinedIcon />
+              </span>
+              <div className="ContactCardInfo">
+                <h3>Address</h3>
+                <p>Uganda, Kampala</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Contact;
