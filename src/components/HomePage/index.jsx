1d44d3df0d9b7b3a9ce2b99a22d10853fe01c9a7
mubangizi <mubangizia22@gmail.com>
2020-11-10 19:17:13 +0300

    Add social Icons

--- src/components/HomePage/index.jsx
+++ src/components/HomePage/index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./Home.css";
 import Layout from "../Layout";
+import GitLightIcon from "../../assets/icons/github_light.svg";
+import LinkedInLightIcon from "../../assets/icons/linkedin.svg";
+import TwitterLightIcon from "../../assets/icons/twitter.svg";
+import { IconButton } from "@material-ui/core";
 
 function Home() {
   return (
@@ -12,7 +16,17 @@ function Home() {
           perfect design. I also make websites interactive and write organised
           REST APIs.
         </p>
-        <div className="MySocial"></div>
+        <div className="MySocial">
+          <IconButton>
+            <img src={GitLightIcon} alt="" />
+          </IconButton>
+          <IconButton>
+            <img src={LinkedInLightIcon} alt="" />
+          </IconButton>
+          <IconButton>
+            <img src={TwitterLightIcon} alt="" />
+          </IconButton>
+        </div>
       </div>
     </Layout>
   );
